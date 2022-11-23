f10cb4111879cdf6920160d756030cc712d325fe
revaadwi <xrevadf@gmail.com>
2022-11-23 18:26:47 +0700

    ulangan harian kk4c (modul 1-17)

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const userrouter = require('./router/users')
+const port = 3000
+const connectDb = require('./config/db')
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(userrouter)
+
+connectDb()
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
